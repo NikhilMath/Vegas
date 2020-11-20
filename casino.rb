10430d2abd58963cf564ef51dbcaa88079750a11
Nikhil Math <erulemath@gmail.com>
2020-11-19 20:19:11 -0700

    created 6 files

---
+++ casino.rb
@@ -0,0 +1,19 @@
+def main_menu
+    puts "welcome to the DPL Casino Roulette resort"
+    puts "please choose a number 1-36 or red/black"
+    user_bet
+end
+
+def user_bet
+    if user_bet == black
+
+
+
+
+main_menu
+
+
+rand(10) => a random number between 0 and 9
+
+color = 100% payout (other than green)
+number = 3500% payout
